5d7b89c6d53101b096bb2c4ff11fa142f13b5b81
cmyannette <132402329+cmyannette@users.noreply.github.com>
2025-03-08 18:41:37 -0600

    Lobby creation via socket backend implementation

--- server/server.js
+++ server/server.js
@@ -1,23 +1,21 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const expressServer = app.listen(3000)
+const expressServer = app.listen(3000);
+const socketio = require("socket.io");
 
-const socketio = require('socket.io')
 const io = socketio(expressServer, {
-    cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"],
-    }
-})
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
-io.on('connect', (socket) => {
-    console.log(socket.id, " has joined our server!")
-    
-    socket.emit('welcome', "Welcome to the server!")
-
-    socket.on('thanks', (data) => {
-        console.log(data)
-    })
-})
+io.on("connect", (socket) => {
+  socket.on("createLobby", () => {
+    const lobbyID = Math.random().toString(36).substr(2, 6).toUpperCase();
+    socket.join(lobbyID); // This line creates (or joins) the room
+    io.to(socket.id).emit("lobbyCreated", { lobbyID });
+  });
+});
