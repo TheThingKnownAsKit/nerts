a58394fc23fd7866680ea60e4a0718adf4308183
Ella Moody <moodyellam@gmail.com>
2025-03-11 16:53:38 -0500

    database linked and working

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Letters from "../components/Letters";
 import "./Signup.css";
 import CustomTextInput from "../components/CustomTextInput";
 import CustomButton from "../components/CustomButton";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase/config";
+import { auth, db } from "../firebase/config";
+import { doc, setDoc } from "firebase/firestore";
 
 function Signup() {
   const navigate = useNavigate();
@@ -18,37 +18,60 @@ function Signup() {
   const handleSignup = async (e) => {
     e.preventDefault();
 
-    //TODO: This might need to change to actual username and email stuff
-    const email = username;
-
     try {
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       );
-      console.log("User signed up:", userCredential.user);
+      const user = userCredential.user;
+      console.log("User signed up:", user);
+
+      // New code for database stuff
+      const userStatisticsRef = doc(db, "user_statistics", user.uid);
+      const userRef = doc(db, "users", user.uid);
+
+      const userData = {
+        uid: user.uid,
+        email: user.email,
+        username: username,
+        statistics: userStatisticsRef,
+      };
+      await setDoc(userRef, userData);
+      console.log("User document created in Firestore");
+
+      const userStatistics = {
+        gamesPlayed: 0,
+        wins: 0,
+        losses: 0,
+        cards_played: 0,
+        time_played: 0,
+        nerts_called: 0,
+      };
+      await setDoc(userStatisticsRef, userStatistics);
+      console.log("User statistics document created in Firestore");
+
       navigate("/home");
     } catch (error) {
       console.error("Error signing up:", error.message);
     }
   };
 
-    // Signup on enter-press
-    let keyPressed = false;
-    document.addEventListener("keydown", (e) => {
-      if (e.key === "Enter" && !keyPressed) {
-        keyPressed = true;
-        if (email && username && password && confirmPassword) {
-          handleSignup(e);
-        }
+  // Signup on enter-press
+  let keyPressed = false;
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && !keyPressed) {
+      keyPressed = true;
+      if (email && username && password && confirmPassword) {
+        handleSignup(e);
       }
-    });
-    document.addEventListener("keyup", (e) => {
-      if (e.key === "Enter") {
-        keyPressed = false;
-      }
-    });
+    }
+  });
+  document.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+      keyPressed = false;
+    }
+  });
 
   return (
     <div className="main centered">
