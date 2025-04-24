a3eea0aa07222556ab1f0fa5da1f6e25aeb7702a
cyannette <cyannette2@huskers.unl.edu>
2025-04-24 15:38:38 -0500

    Initial Jest unit testing implementation

---
+++ server/src/testing/gameManager.test.js
@@ -0,0 +1,9 @@
+import GameManager from "../game/gameManager";
+
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+test("test", () => {
+  expect(1).toBe(1);
+});
---
+++ server/src/testing/hand.test.js
@@ -0,0 +1,9 @@
+import Hand from "../models/hand.js";
+
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+test("test", () => {
+  expect(1).toBe(1);
+});
