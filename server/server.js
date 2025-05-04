0b15a4fd3637bece850b2ef41cde71ee2ebc844f
Carter Yannette <cyannette2@huskers.unl.edu>
2025-05-03 23:04:49 -0500

    Made running the server easier by automatically building front end when running `npm run server` (still allows for development editing with `npm run start` on port 5173. Port 3000 now serves static files

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ const __dirname = path.dirname(__filename);
 // Create an Express application instance and pass in as handler for HTTP server instance
 const app = express();
 const server = createServer(app);
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 3000; // Get port being run on or default to 3000
 
 const gameManager = new GameManager(); // Initialize game manager
 
@@ -40,12 +41,13 @@ io.on("connect", (socket) => {
 lobbySocket(io, gameManager);
 gameSocket(io, gameManager);
 
+
 app.use(express.static(path.join(__dirname, "dist"))); // Serve static front-end compiled files
 
 // For any requests not found in compiled static files, revert to index.html
 app.get("*", (request, response) => {
   response.sendFile(path.join(__dirname, "dist", "index.html"));
-})
+});
 
 // Start HTTP server on port 3000 and log
 server.listen(port, () => console.log(`Server running on port ${port}`));
