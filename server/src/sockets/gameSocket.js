e911a87a3e7de08e0c7510e2b5a462c5bbc493f7
cyannette <cyannette2@huskers.unl.edu>
2025-05-08 20:19:44 -0500

    FIxed bug where timer did no reset in between rounds

--- server/src/sockets/gameSocket.js
+++ server/src/sockets/gameSocket.js
@@ -1,25 +1,25 @@
 export default (io, gameManager) => {
-    io.on("connect", (socket) => {
-        // Event listener for starting a game
-        socket.on("startGame", (lobbyId) => {
-            const socketsInRoom = io.sockets.adapter.rooms.get(lobbyId); // Set of socket IDs
-            const players = [];
+  io.on("connect", (socket) => {
+    // Event listener for starting a game
+    socket.on("startGame", (lobbyId) => {
+      const socketsInRoom = io.sockets.adapter.rooms.get(lobbyId); // Set of socket IDs
+      const players = [];
 
-            for (const socketId of socketsInRoom) {
-                const playerSocket = io.sockets.sockets.get(socketId);
-                if (playerSocket?.data?.userID) {
-                    players.push(playerSocket.data.userID);
-                }
-            }
+      for (const socketId of socketsInRoom) {
+        const playerSocket = io.sockets.sockets.get(socketId);
+        if (playerSocket?.data?.userID) {
+          players.push(playerSocket.data.userID);
+        }
+      }
 
-            if (players.length > 4 || players.length < 1) return; // Not enough players to start the game)
+      if (players.length > 4 || players.length < 1) return; // Not enough players to start the game)
 
-            const gameState = gameManager.startGame(lobbyId, players); // Now using userIDs
-            io.to(lobbyId).emit("gameStarted", gameState);
-        });
+      const gameState = gameManager.startGame(lobbyId, players); // Now using userIDs
+      io.to(lobbyId).emit("gameStarted", gameState);
+    });
 
-        // Event listener for playing a card
-        /* EXAMPLE PAYLOAD
+    // Event listener for playing a card
+    /* EXAMPLE PAYLOAD
     {
       source: {
         card: {
@@ -36,74 +36,79 @@ export default (io, gameManager) => {
       playerId: "XTywdu5KJNsrKqTcAAAB",
       lobbyId: "ABCDEF"
     } */
-        socket.on("cardPlayed", (payload) => {
-            const gameState = gameManager.games[payload.lobbyId];
-            const moveWasMade = gameState.playCard(payload, gameManager);
+    socket.on("cardPlayed", (payload) => {
+      const gameState = gameManager.games[payload.lobbyId];
+      const moveWasMade = gameState.playCard(payload, gameManager);
 
-            socket.emit("cardPlayAccepted", moveWasMade);
+      socket.emit("cardPlayAccepted", moveWasMade);
 
-            if (moveWasMade) {
-                io.to(payload.lobbyId).emit("gameStateUpdated", gameState);
-            }
-        });
-
-        // Event listener for flipping the draw pile
-        /* EXAMPLE PAYLOAD
-    {
-      lobbyId: "ABCDEF",
-      playerId: "XTywdu5KJNsrKqTcAAAB"
-    } */
-        socket.on("flipDrawPile", (payload) => {
-            let gameState = gameManager.games[payload.lobbyId]; // Get game state of specified lobby
-            const card = gameState.flipDrawPile(payload.playerId); // Get new "visible" draw pile card
-            const playerId = payload.playerId; // Get player ID
-
-            socket.emit("newDrawCard", { card, playerId }); // Send this updated card
-            io.to(payload.lobbyId).emit("newDrawCard", {
-                card,
-                playerId,
-            });
-            // Trigger refresh manually because the flip function only modifies a primitive type
-            const safeState = JSON.parse(JSON.stringify(gameManager.games[payload.lobbyId]));
-            io.to(payload.lobbyId).emit("gameStateUpdated", safeState);
-        });
-
-        // Event listener for calling nerts
-        /* EXAMPLE PAYLOAD
-    {
-      lobbyId: "ABCDEF",
-      playerId: "XTywdu5KJNsrKqTcAAAB"
-    } */
-        socket.on("callNerts", (payload) => {
-            const gameState = gameManager.games[payload.lobbyId]; // Get game state of specified lobby
-            const endRound = gameState.callNerts(payload.playerId); // Attempt to call nerts
-
-            // Check if round has ended
-            if (endRound) {
-                const players = gameState.getPlayers(); // Get array of all player objects
-
-                // Check if someone is above 100 points at end of round
-                const scoreAboveThreshold = players.some((player) => player.score >= 10);
-
-                // Broadcast that game has ended if a player won and send scores
-                if (scoreAboveThreshold) {
-                    io.to(payload.lobbyId).emit("endGame", { players });
-                } else {
-                    io.to(payload.lobbyId).emit("endRound"); // Broadcast that round has ended
-                }
-            }
-        });
-
-        // Event listener for starting another round
-        socket.on("startRound", (lobbyId) => {
-            const gameState = gameManager.games[lobbyId];
-            gameState.startRound();
-
-            // Trigger refresh manually because the flip function only modifies a primitive type
-            const safeState = JSON.parse(JSON.stringify(gameState));
-            io.to(lobbyId).emit("gameStateUpdated", safeState);
-
-            io.to(lobbyId).emit("roundStarted");
-        });
+      if (moveWasMade) {
+        io.to(payload.lobbyId).emit("gameStateUpdated", gameState);
+      }
     });
+
+    // Event listener for flipping the draw pile
+    /* EXAMPLE PAYLOAD
+    {
+      lobbyId: "ABCDEF",
+      playerId: "XTywdu5KJNsrKqTcAAAB"
+    } */
+    socket.on("flipDrawPile", (payload) => {
+      let gameState = gameManager.games[payload.lobbyId]; // Get game state of specified lobby
+      const card = gameState.flipDrawPile(payload.playerId); // Get new "visible" draw pile card
+      const playerId = payload.playerId; // Get player ID
+
+      socket.emit("newDrawCard", { card, playerId }); // Send this updated card
+      io.to(payload.lobbyId).emit("newDrawCard", {
+        card,
+        playerId,
+      });
+      // Trigger refresh manually because the flip function only modifies a primitive type
+      const safeState = JSON.parse(
+        JSON.stringify(gameManager.games[payload.lobbyId])
+      );
+      io.to(payload.lobbyId).emit("gameStateUpdated", safeState);
+    });
+
+    // Event listener for calling nerts
+    /* EXAMPLE PAYLOAD
+    {
+      lobbyId: "ABCDEF",
+      playerId: "XTywdu5KJNsrKqTcAAAB"
+    } */
+    socket.on("callNerts", (payload) => {
+      const gameState = gameManager.games[payload.lobbyId]; // Get game state of specified lobby
+      const endRound = gameState.callNerts(payload.playerId); // Attempt to call nerts
+
+      // Check if round has ended
+      if (endRound) {
+        const players = gameState.getPlayers(); // Get array of all player objects
+
+        // Check if someone is above 100 points at end of round
+        const scoreAboveThreshold = players.some(
+          (player) => player.score >= 10
+        );
+
+        // Broadcast that game has ended if a player won and send scores
+        if (scoreAboveThreshold) {
+          io.to(payload.lobbyId).emit("endGame", { players });
+        } else {
+          io.to(payload.lobbyId).emit("endRound"); // Broadcast that round has ended
+        }
+      }
+    });
+
+    // Event listener for starting another round
+    socket.on("startRound", (lobbyId) => {
+      const gameState = gameManager.games[lobbyId];
+      gameState.startRound();
+
+      // Trigger refresh manually because the flip function only modifies a primitive type
+      const safeState = JSON.parse(JSON.stringify(gameState));
+      io.to(lobbyId).emit("gameStateUpdated", safeState);
+
+      io.to(lobbyId).emit("roundStarted");
+      gameManager.resetShuffleTimers(lobbyId);
+    });
+  });
 };
